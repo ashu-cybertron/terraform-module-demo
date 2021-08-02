f07e39a0f626f3cd24afe152b81c8f5befa87dd7
NAME <anandashutosh563@gmail.com>
2021-08-02 18:32:29 +0530

    keypair name changes

--- dev/main.tf
+++ dev/main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = "ap-south-1"
   #according to OS give the path of aws credentials i copied in accessible folder to ease of access
   shared_credentials_file = "/var/lib/jenkins/workspace/jenkins-terrraform/cred"
-  profile = "default"
+  profile                 = "default"
 }
 module "my_vpc" {
 
-  source                      = "../modules/vpc"
-  vpc_cidr_block              = "10.0.0.0/16"
-  vpc_subnet_cidr_block       = "10.0.1.0/24"
+  source                = "../modules/vpc"
+  vpc_cidr_block        = "10.0.0.0/16"
+  vpc_subnet_cidr_block = "10.0.1.0/24"
 
-  vpc_id                      = "${module.my_vpc.vpc_id}"
-  vpc_subnet_id               = "${module.my_vpc.vpc_subnet_id}"
-  vpc_aws_internet_gateway_id = "${module.my_vpc.vpc_aws_internet_gateway_id}"
-  vpc_route_table_id          = "${module.my_vpc.vpc_route_table_id}"
+  vpc_id                      = module.my_vpc.vpc_id
+  vpc_subnet_id               = module.my_vpc.vpc_subnet_id
+  vpc_aws_internet_gateway_id = module.my_vpc.vpc_aws_internet_gateway_id
+  vpc_route_table_id          = module.my_vpc.vpc_route_table_id
 
 }
 
@@ -22,11 +22,11 @@ module "my_ec2" {
   source                = "../modules/ec2"
   ec2_ami               = "ami-0c1a7f89451184c8b"
   ec2_availability_zone = "ap-south-1a"
-  ec2_key_name          = "win"
+  ec2_key_name          = "jenkins"
 
 }
 
-output "ec2_ip"{
+output "ec2_ip" {
 
-  value="${module.my_ec2.ec2_public_ip}"
+  value = module.my_ec2.ec2_public_ip
 }
--- prod/main.tf
+++ prod/main.tf
@@ -1,25 +1,25 @@
 provider "aws" {
-  region                  = "ap-south-1"
+  region = "ap-south-1"
   shared_credentials_file = "/var/lib/jenkins/workspace/jenkins-terrraform/cred"
-  profile                 = "default"
+  profile = "default"
 }
 module "my_vpc" {
 
   source                      = "../modules/vpc"
-  vpc_cidr_block              = var.vpc_cidr_block
-  vpc_subnet_cidr_block       = var.vpc_subnet_cidr_block
-  vpc_id                      = module.my_vpc.vpc_id
-  vpc_subnet_id               = module.my_vpc.vpc_subnet_id
-  vpc_aws_internet_gateway_id = module.my_vpc.vpc_aws_internet_gateway_id
-  vpc_route_table_id          = module.my_vpc.vpc_route_table_id
+  vpc_cidr_block              = "${var.vpc_cidr_block}"
+  vpc_subnet_cidr_block       = "${var.vpc_subnet_cidr_block}"
+  vpc_id                      = "${module.my_vpc.vpc_id}"
+  vpc_subnet_id               = "${module.my_vpc.vpc_subnet_id}"
+  vpc_aws_internet_gateway_id = "${module.my_vpc.vpc_aws_internet_gateway_id}"
+  vpc_route_table_id          = "${module.my_vpc.vpc_route_table_id}"
 
 }
 
 module "my_ec2" {
 
   source                = "../modules/ec2"
-  ec2_ami               = var.ec2_ami
-  ec2_availability_zone = var.ec2_availability_zone
-  ec2_key_name          = var.ec2_key_name
+  ec2_ami               = "${var.ec2_ami}"
+  ec2_availability_zone = "${var.ec2_availability_zone}"
+  ec2_key_name          = "${var.ec2_key_name}"
 
 }
